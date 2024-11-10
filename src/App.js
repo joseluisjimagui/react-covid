2da3a779717c95fcc7ff47ac0e59624bc8006ea8
Jose Jimenez <joseluisjimagui@gmail.com>
2024-11-10 16:43:37 -0600

    Add React router and principal page

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
 import Header from './components/Header';
-import Footer from './components/Footer'
+import Footer from './components/Footer';
+import PrincipalMenu from './components/PrincipalMenu';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom/cjs/react-router-dom.min';
 
 function App() {
   return (
-    <div className="App">      
-      <Header></Header>
-      <h1>Main Content</h1>
-      <Footer></Footer>
-    </div>
+    <Router>
+      <div className="App">      
+        <Header />
+        <h1>Main Content</h1>
+        <Switch>
+          <Route path="/"> <PrincipalMenu /> </Route>
+        </Switch>
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
