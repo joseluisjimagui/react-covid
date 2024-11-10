55eb7d0c88ee9b5f51c20c008785c1a67daac003
Jose Jimenez <joseluisjimagui@gmail.com>
2024-11-09 23:17:01 -0600

    Add footer

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import Header from './components/Header';
+import Footer from './components/Footer'
 
 function App() {
   return (
     <div className="App">      
       <Header></Header>
-      <h1>Sample Title</h1>
+      <h1>Main Content</h1>
+      <Footer></Footer>
     </div>
   );
 }
---
+++ src/components/Footer.js
@@ -0,0 +1,15 @@
+const Footer = () => {
+    return (
+        <footer class="bg-body-tertiary text-center">            
+
+            <div class="text-center p-3" style={{background: 'rgba(0, 0, 0, 0.05)'}}>
+                © 2024 Copyright: 
+                <a class="text-body" href="https://covidtracking.com/data/api/">Click here to go to the API Web Page</a>
+            </div>
+        </footer>
+    );
+}
+
+
+
+export default Footer;
